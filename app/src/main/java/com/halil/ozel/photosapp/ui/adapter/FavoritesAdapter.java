6ca87deab0cac3d2ee0f02270309b799cef9140d
Halil Ozel <halilozel1903@gmail.com>
2025-10-18 12:36:07 +0300

    Add FavoritesAdapter to manage and display favorite photos in a RecyclerView

---
+++ app/src/main/java/com/halil/ozel/photosapp/ui/adapter/FavoritesAdapter.java
@@ -0,0 +1,89 @@
+package com.halil.ozel.photosapp.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.halil.ozel.photosapp.R;
+import com.halil.ozel.photosapp.data.local.FavoritePhoto;
+import com.halil.ozel.photosapp.databinding.ItemFavoritePhotoBinding;
+import com.halil.ozel.photosapp.utils.ImageLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavoritesAdapter extends RecyclerView.Adapter<FavoritesAdapter.FavoriteViewHolder> {
+
+    private final Context context;
+    private final OnFavoriteClickListener clickListener;
+    private final OnRemoveFavoriteListener removeListener;
+    private List<FavoritePhoto> favoritesList = new ArrayList<>();
+
+    public interface OnFavoriteClickListener {
+        void onFavoriteClick(String photoUrl);
+    }
+
+    public interface OnRemoveFavoriteListener {
+        void onRemoveFavorite(String photoId);
+    }
+
+    public FavoritesAdapter(Context context, OnFavoriteClickListener clickListener, OnRemoveFavoriteListener removeListener) {
+        this.context = context;
+        this.clickListener = clickListener;
+        this.removeListener = removeListener;
+    }
+
+    @NonNull
+    @Override
+    public FavoriteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemFavoritePhotoBinding binding = ItemFavoritePhotoBinding.inflate(
+                LayoutInflater.from(parent.getContext()), parent, false);
+        return new FavoriteViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavoriteViewHolder holder, int position) {
+        FavoritePhoto photo = favoritesList.get(position);
+        holder.bind(photo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return favoritesList.size();
+    }
+
+    public void submitList(List<FavoritePhoto> favorites) {
+        this.favoritesList = new ArrayList<>(favorites);
+        notifyDataSetChanged();
+    }
+
+    public class FavoriteViewHolder extends RecyclerView.ViewHolder {
+        private final ItemFavoritePhotoBinding binding;
+
+        public FavoriteViewHolder(@NonNull ItemFavoritePhotoBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(FavoritePhoto photo) {
+            ImageLoader.loadImage(binding.ivPhoto, photo.getUrl());
+            binding.tvTitle.setText(photo.getTitle());
+
+            binding.getRoot().setOnClickListener(v -> {
+                if (clickListener != null) {
+                    clickListener.onFavoriteClick(photo.getUrl());
+                }
+            });
+
+            binding.btnRemove.setOnClickListener(v -> {
+                if (removeListener != null) {
+                    removeListener.onRemoveFavorite(photo.getId());
+                }
+            });
+        }
+    }
+}
+
