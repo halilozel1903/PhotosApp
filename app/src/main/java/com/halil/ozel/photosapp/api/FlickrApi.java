6de36ee8daa4870c978801e17bfea76a2f6ff18b
Halil Ozel <halilozel1903@gmail.com>
2025-10-18 12:35:24 +0300

    Refactor FlickrApi to use Constants for base URL and timeouts; mark class as deprecated

--- app/src/main/java/com/halil/ozel/photosapp/api/FlickrApi.java
+++ app/src/main/java/com/halil/ozel/photosapp/api/FlickrApi.java
@@ -1,17 +1,44 @@
 package com.halil.ozel.photosapp.api;
 
+import com.halil.ozel.photosapp.utils.Constants;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @deprecated This class is deprecated and should not be used.
+ * Use Hilt Dependency Injection with NetworkModule instead.
+ * See {@link com.halil.ozel.photosapp.di.NetworkModule} for proper DI setup.
+ */
+@Deprecated
 public class FlickrApi {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://www.flickr.com/services/rest/";
 
+    /**
+     * @deprecated Use Hilt injection instead. Inject Retrofit via NetworkModule.
+     */
+    @Deprecated
     public static Retrofit getRetrofitInstance() {
-
         if (retrofit == null) {
-            retrofit = new retrofit2.Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+            // Logging interceptor for debugging
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            // OkHttp client with timeouts
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(loggingInterceptor)
+                    .connectTimeout(Constants.CONNECT_TIMEOUT, TimeUnit.SECONDS)
+                    .readTimeout(Constants.READ_TIMEOUT, TimeUnit.SECONDS)
+                    .writeTimeout(Constants.WRITE_TIMEOUT, TimeUnit.SECONDS)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Constants.BASE_URL)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
