d3ff28f4cd5f76024b545f2d7b2986fc13ef98c7
Halil Ozel <halilozel1903@gmail.com>
2025-10-18 12:35:16 +0300

    Refactor FlickrService to use Constants for API key management

--- app/src/main/java/com/halil/ozel/photosapp/api/FlickrService.java
+++ app/src/main/java/com/halil/ozel/photosapp/api/FlickrService.java
@@ -2,21 +2,21 @@ package com.halil.ozel.photosapp.api;
 
 import com.halil.ozel.photosapp.data.ResponsePhoto;
 import com.halil.ozel.photosapp.data.ResponsePhotos;
+import com.halil.ozel.photosapp.utils.Constants;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface FlickrService {
-    @GET("?method=flickr.photos.getRecent&api_key=5adbb887ee60c548c02690ef16b848cd" +
+    @GET("?method=flickr.photos.getRecent&api_key=" + Constants.API_KEY +
             "&format=json&nojsoncallback=1")
     Call<ResponsePhotos> getResponse(
             @Query("per_page") int per_page,
             @Query("page") int page
 
     );
-    @GET("?method=flickr.photos.getInfo" +
-            "&api_key=5adbb887ee60c548c02690ef16b848cd" + "" +
+    @GET("?method=flickr.photos.getInfo&api_key=" + Constants.API_KEY +
             "&format=json&nojsoncallback=1")
     Call<ResponsePhoto> getPhotoInfo(
             @Query("photo_id") String photo_id
